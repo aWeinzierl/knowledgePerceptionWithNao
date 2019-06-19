0e950d9168417f5cfb17753d29112c6cdb283172
Andreas Weinzierl <20560137+aWeinzierlo@users.noreply.github.com>
2019-06-19 22:03:29 +0200

    add possibility to create and query time instants, register start times, register motions

--- src/PrologClient.cpp
+++ src/PrologClient.cpp
@@ -17,9 +17,53 @@ uint PrologClient::CreateTypeInstance(const std::string &associatedClass) {
 
     class_to_current_id_associations_[associatedClass] = supposedId;
 
-    for (auto const & bdg : bdgs){
-        std::cout << bdg["ObjInst"] << "\n";
-    }
+    logQueryResult(bdgs);
 
     return supposedId;
+}
+
+void PrologClient::logQueryResult(json_prolog::PrologQueryProxy &bdgs) const {
+    for (auto const &bdg : bdgs) {
+        std::cout << bdg["ObjInst"] << "\n";
+    }
+}
+
+void PrologClient::Register_motion_for_object(const Instance &instance, uint timeInstant,
+                                              DIRECTION direction) {
+    auto bdgs = _pl.query(
+            "rdf_assert('http://knowrob.org/kb/Nao_computables.owl#Carrot" + instance.Get_class() +"_"+ std::to_string(instance.Get_id()) +"',"+
+            "knowrob:startTime, 'http://knowrob.org/kb/knowrob.owl#TimePoint_"+ std::to_string(timeInstant) +"')");
+    logQueryResult(bdgs);
+}
+
+void PrologClient::CreateTimePoint(uint timeInstant) {
+
+    if (Time_point_already_exists(timeInstant)) {
+        return;
+    }
+
+    auto bdgs = _pl.query(
+            "rdf_costom_instance_from_class('http://knowrob.org/kb/knowrob.owl#TimePoint',_," +
+            std::to_string(timeInstant) + ",ObjInst)");
+    logQueryResult(bdgs);
+}
+
+bool PrologClient::Time_point_already_exists(uint timeInstant) {
+    auto bdgs = _pl.query(
+            "owl_individual_of('http://knowrob.org/kb/knowrob.owl#TimePoint_" + std::to_string(timeInstant) + "','http://knowrob.org/kb/knowrob.owl#TimePoint')"
+            );
+
+    return bdgs.begin()==bdgs.begin();
+}
+
+
+PrologClient::Instance::Instance(const std::string &classType, uint id) : _class(classType),_id(id) {
+}
+
+const std::string &PrologClient::Instance::Get_class() const noexcept {
+    return _class;
+}
+
+uint PrologClient::Instance::Get_id() const noexcept {
+    return _id;
 }
--- src/PrologClient.h
+++ src/PrologClient.h
@@ -10,10 +10,28 @@ class PrologClient {
     std::unordered_map<std::string, unsigned int> class_to_current_id_associations_;
     json_prolog::Prolog _pl;
 
+    struct Instance{
+        std::string _class;
+        uint _id;
+
+        Instance(const std::string& classType, uint id);
+        const std::string& Get_class() const noexcept;
+        uint Get_id() const noexcept;
+    };
+
 public:
 
     PrologClient();
 
     uint CreateTypeInstance(const std::string &associatedClass);
 
+    void Register_motion_for_object(const Instance &instance, uint timeInstant, DIRECTION direction);
+
+    void Register_start_time_for_instance(const Instance &instance, uint timeInstant);
+
+    bool Time_point_already_exists(uint timeInstant);
+
+    void CreateTimePoint(uint timeInstant);
+
+    void logQueryResult(json_prolog::PrologQueryProxy &bdgs) const;
 };
